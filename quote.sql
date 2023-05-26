07fd464bea7ebda052e018e1d4e5fa224c0afad1
safaa-mammed <73444223+safaa-mammed@users.noreply.github.com>
2023-05-26 19:04:40 +0800

    first

---
+++ quote.sql
@@ -0,0 +1,5 @@
+CREATE TABLE quote (id int, msg varchar(255));
+INSERT INTO quote VALUES (1, "Quote 1..........");
+INSERT INTO quote VALUES (2, "Quote 2..........");
+INSERT INTO quote VALUES (3, "Quote 3..........");
+INSERT INTO quote VALUES (4, "Quote 4..........");
